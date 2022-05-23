550a2c3ad2efbd8c771b59cc9f04ebb6e76835bb
Mohammad Hawa <66217824+mohammdhawa@users.noreply.github.com>
2022-05-23 17:31:40 +0300

    Create Ex04.java

---
+++ Java_OOP/Relationships/Ex04.java
@@ -0,0 +1,25 @@
+
+interface BirInterface {
+	
+	public void metot1();
+	
+}
+
+class BaskaBirSinif {
+	
+}
+
+public class BirSinif extends BaskaBirSinif implements BirInterface {
+	
+	private int d1;
+	private String d2;
+	
+	public void metot1() {
+		
+	}
+	
+	public String metot2() {
+		return null;
+	}
+	
+}
